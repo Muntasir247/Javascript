788ed6578433ed86abc12c4e425822b68d26aa64
Muntasir247 <adormuntasir@gmail.com>
2026-01-27 21:07:04 +0600

    Day 1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("HI");
-alert("Hi");
+console.log("console log");
+console.warn("warning");
+console.error("error");
+console.info("info");
+console.log("%cHello", "color:red");
+// console.clear(); to clear console
+console.error("error", 2 - 1);
+console.table({ name: "Muntasir", age: 69 });
